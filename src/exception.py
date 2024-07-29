bca3d8e3814a3fc324a5d421b11ce64c84dec64a
Amit Mahindra <Amythcode@gmail.com>
2024-07-29 23:52:56 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_, exc_tb = error_detail.exc_info()
@@ -19,3 +19,10 @@ class CustomException(Exception):
         return self.error_message
     
 
+if __name__ == '__main__':
+     
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise CustomException(e, sys)
